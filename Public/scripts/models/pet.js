228c045ff88bfe762a948eb06dd80870de9994d2
zachar13y <zachar13y@gmail.com>
2018-11-28 19:54:55 -0800

    Made pet constructor function.

---
+++ Public/scripts/models/pet.js
@@ -0,0 +1,12 @@
+ function petData(img, breed, sex, name, age, color, size, temperment) {
+    this.img = img.src;
+    this.breed = breed;
+    this.sex = sex;
+    this.name = name;
+    this.age = age;
+    this.color = color;
+    this.size = size;
+    this.temperment = temperment
+}
+
+
