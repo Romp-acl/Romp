d7a8bf6e4543989da821d2a3bffae881a6cd8686
Kate Dameron <katerj33@gmail.com>
2018-12-04 15:42:58 -0800

    changed the profile constructor to include a pets array and the petProfile constructor

--- Public/scripts/models/pet.js
+++ Public/scripts/models/pet.js
@@ -1,21 +1,21 @@
- function PetProfile(img, breed, sex, name, age, color, size, temperment, interests) {
-    this.img = img;
-    this.breed = breed;
-    this.sex = sex;
-    this.name = name;
-    this.age = age;
-    this.color = color;
-    this.size = size;
-    this.temperment = temperment;
-    this.interest = interests;
-    this.about = about;
-}
+//  function PetProfile(img, breed, sex, name, age, color, size, temperment, interests) {
+//     this.img = img;
+//     this.breed = breed;
+//     this.sex = sex;
+//     this.name = name;
+//     this.age = age;
+//     this.color = color;
+//     this.size = size;
+//     this.temperment = temperment;
+//     this.interest = interests;
+//     this.about = about;
+// }
 
-PetProfile.all = [];
+// PetProfile.all = [];
 
-function addPet(rawData) {
-    PetProfile.all.push(new petProfile(rawData));
-}
+// function addPet(rawData) {
+//     PetProfile.all.push(new petProfile(rawData));
+// }
 
 
 
--- Public/scripts/models/profile.js
+++ Public/scripts/models/profile.js
@@ -3,13 +3,24 @@ function UserProfile(username, password, email, address) {
     this.password = password;
     this.email = email;
     this.address = address;
+    this.newPet = function PetProfile(img, breed, sex, name, age, color, size, temperment, interests) {
+        this.img = img;
+        this.breed = breed;
+        this.sex = sex;
+        this.name = name;
+        this.age = age;
+        this.color = color;
+        this.size = size;
+        this.temperment = temperment;
+        this.interest = interests;
+        this.about = about;
+    }
+    this.pets = [];
 }
 
 UserProfile.all = [];
 
-function addUser(rawData) {
-    UserProfile.all.push(new UserProfile(rawData));
-}
+
 
 
 
