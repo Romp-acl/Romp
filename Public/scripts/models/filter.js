2424d0ba7029ed985f16350adfc59caef793b2d8
zachar13y <zachar13y@gmail.com>
2018-12-05 12:10:16 -0800

    Put images in assets folder, hid filter, fixed indentation in css files, added hover to submit input.

--- Public/scripts/models/filter.js
+++ Public/scripts/models/filter.js
@@ -1,6 +1,5 @@
 $('.filter button').on('click', function(){$('.filterPopup').toggleClass('active')});
-$('.signupPopup li').on('click', function(e){
     const targetText = e.target.innerText.toLowerCase();
     $(`.heroForm`).removeClass('active');
     $(`.${targetText}`).addClass('active');
-})
+// })
