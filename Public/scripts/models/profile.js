520f56199b73e522527b21cf8fd292f5cb1413d8
idomskt <idomskt@gmail.com>
2018-12-06 13:30:37 -0800

    single marker is working

--- Public/scripts/models/profile.js
+++ Public/scripts/models/profile.js
@@ -3,6 +3,7 @@ function UserProfile(user) {
     this.password = user.password;
     this.email = user.email;
     this.address = user.address;
+    this.id = user.owner_id;
     this.petObj = {
         imgUrl : user.imgUrl,
         breed : user.breed,
@@ -25,6 +26,7 @@ UserProfile.loadUsers = function() {
             UserProfile.all.push(new UserProfile(user));
         });
     })
+    getAddresses();
 }
 
 UserProfile.prototype.toHtml = function() {
@@ -39,8 +41,17 @@ UserProfile.prototype.toHtml = function() {
 
 
 
-
-
+//loop through the petsinfo array to find the coords and match it with the user address.
+//then, add it to the UserProfile
+function matchCoords() {
+    UserProfile.all.map(eachProfile => {
+        petsInfo.map(eachPet => {
+            if(eachProfile.address == eachPet.address){
+                return eachProfile.coords = eachPet.coordinates;
+            }
+        })
+    })
+}
 
 
 
--- Public/scripts/views/mapView.js
+++ Public/scripts/views/mapView.js
@@ -1,13 +1,10 @@
 var markers = [];
-var filter = ["black"];
 
 function initMap() {
-    var portland = {lat: 45.5122, lng: -122.6587};
     var map = new google.maps.Map(document.getElementsByClassName('map')[0], {
         zoom: 10, 
         center: new google.maps.LatLng(45.5122, -122.6587),
     });
-    getAddresses();
     
     setTimeout(function(){ 
         for (var i=0; i < petsInfo.length; i++) {
@@ -16,15 +13,96 @@ function initMap() {
                 map: map, 
                 title: petsInfo[i].name,
                 properties: {
+                    owner_id: petsInfo[i].owner_id,
                     color: petsInfo[i].color, 
                     breed: petsInfo[i].breed,
                     sex: petsInfo[i].sex,
                     age: petsInfo[i].age,
+                    img: petsInfo[i].imgurl.split('Public/')[1],
                 }
             }));
         };
-    }, 3000);
+        for(var m=0; m < markers.length; m++) {
+            markers[m].addListener('click', buildMarkerPopup);
+            }
+    }, 1000);
+    
+    function buildMarkerPopup() {
+        var markerContent = '<div id="content">'+
+                            '<div id="siteNotice">'+
+                            '</div>'+
+                            '<h1 id="firstHeading" class="firstHeading">'+this.title+'</h1>'+
+                            '<div id="bodyContent">'+
+                            '<img src="'+this.properties.img+'">'+
+                            '<p><b>Age: </b>'+this.properties.age+'<br>'+
+                            '<b>Breed: </b>'+this.properties.breed+'<br>'+
+                            '<b>Color: </b>'+this.properties.color+'<br>'+
+                            '<b>Sex: </b>'+this.properties.sex+'<br>'+
+                            '<button onclick="viewProfileBTN('+this.properties.owner_id+')">View Profile</button></p>'
+                            '</div>'+
+                            '</div>';
+
+        var infowindow = new google.maps.InfoWindow({
+            content: markerContent
+        });
+        infowindow.open(map, this);
+    }
 }
+
+
+
+function initFriendMap(friendProfile) {
+    function initMap() {
+
+        var friendCoords = friendProfile.coords;
+        var map = new google.maps.Map(document.getElementsByClassName('map')[0], {
+            zoom: 10, 
+            center: new google.maps.LatLng(friendCoords),
+        });
+        
+        setTimeout(function(){
+
+            var marker = new google.maps.Marker({
+                position: friendCoords, 
+                map: map,
+                properties: {
+                    Address: friendProfile.address,
+                }
+            });
+            marker.addListener('click', function(){
+                var infowindow = new google.maps.InfoWindow({
+                    content: friendProfile.address
+                });
+                infowindow.open(map, marker);
+            });
+        }, 1000);
+    }
+    initMap();
+}
+
+
+
+
+var friendProfile = [];
+function viewProfileBTN(id) {
+    matchCoords();
+    for (var i = 0; i < UserProfile.all.length; i++) {
+        if (UserProfile.all[i].id == id) {
+            console.log(id);
+            console.log(UserProfile.all[i].username);
+            friendProfile = UserProfile.all[i];
+            $('.hideOnMarkerClick').remove();
+            $('.userProfile').removeClass('hide');
+            $('.userProfile').prepend(UserProfile.all[i].toHtml());
+            $('.hero').hide();
+            $('userProfile').show().find('.map').removeClass('map').addClass('friendMap');
+            initMsgBoard();
+            initFriendMap(friendProfile);
+            window.scrollTo(0, 0);
+        }
+    }
+}
+
 
 function filterMarkers() {
     for (var i = 0; i < filter.length; i++) {
