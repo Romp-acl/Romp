0e1464ab7750e77d963e191e416e13e9cfcc4847
zachar13y <zachar13y@gmail.com>
2018-12-06 16:11:14 -0800

    Changed constring, fixed indentation, added active class for sign up button.

--- Public/scripts/models/filter.js
+++ Public/scripts/models/filter.js
@@ -1,5 +1,2 @@
 $('.filter button').on('click', function(){$('.filterPopup').toggleClass('active')});
-    const targetText = e.target.innerText.toLowerCase();
-    $(`.heroForm`).removeClass('active');
-    $(`.${targetText}`).addClass('active');
-// })
+    
--- Public/scripts/models/login.js
+++ Public/scripts/models/login.js
@@ -1,13 +1,15 @@
 $('.hero button').on('click', function(){
     userLogin()
-    $('.signupPopup').toggleClass('active')});
-    $('.signupPopup li').on('click', function(e){
-        $('.signupPopup li').css('color', 'white');
-        $(this).css('color', 'yellow');
-        const targetText = e.target.innerText.toLowerCase();
-        $(`.heroForm`).removeClass('active');
-        $(`.${targetText}`).addClass('active');
-})
+    $('.signupPopup').addClass('active')
+});
+
+$('.signupPopup li').on('click', function(e){
+    $('.signupPopup li').css('color', 'white');
+    $(this).css('color', 'yellow');
+    const targetText = e.target.innerText.toLowerCase();
+    $(`.heroForm`).removeClass('active');
+    $(`.${targetText}`).addClass('active');  
+});
 
 var $username = "";
 var $loginID = "";
